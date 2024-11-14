6dae56087784b2934208b317807084bf8015eb05
unknown <oldfisherman96@gmail.com>
2024-11-15 01:14:56 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import { DurableObject } from "cloudflare:workers";
 
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
 
 /**
  * Env provides a mechanism to reference bindings declared in wrangler.toml within JavaScript
